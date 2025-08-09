238755c29aab707bd46767161bb6997ff0014a2c
Holinda Beshiri <holinda_b@hotmail.com>
2025-08-09 20:15:46 +0000

    Update styldes and add content to about me modal

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,25 @@
+window.addEventListener('load', () => {
+  const img = document.querySelector('.profile-image img');
+  if (img) {
+    img.classList.add('visible');
+  }
+});
 
-// Get the modal
+// Modal handling
 var modal = document.getElementById("about-me-modal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("about-me-btn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
