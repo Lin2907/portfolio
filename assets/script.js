41b15127d6b0a6f78cc66d417b34839f30b7191b
Holinda Beshiri <holinda_b@hotmail.com>
2025-08-10 15:48:25 +0000

    add media queries and adjust the navigation for small screens

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,12 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    });
+  });
